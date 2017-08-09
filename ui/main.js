6e7b912526712b1bbea0474db5b5acba2449db45
Manodhar-Nainwal <30679208+Manodhar-Nainwal@users.noreply.github.com>
2017-08-09 20:46:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
- 
+console
+//counter code
+var button = document.getElementById('counter');
+
+button.onclick = function() {
+
+
+    // Create a request oblect
+    var reques = new XMLHttpRequest();
+    
+    // Capture the reponse and store it in a variable
+    request.onreadystatchange = function() {
+        if (request.readystat === XMLHttpRequest.DONE) {
+            // Take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+            }
+        }
+        // Not done yet
+        };
+    // Make the request    
+     request.open('GET" http://manodharn.imad.hasura-app.io/counter', true);  
+     request.send(null);   
+};        
