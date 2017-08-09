34630897ce34930b42d7f0e5d68872d2ab2f8bc8
Manodhar-Nainwal <30679208+Manodhar-Nainwal@users.noreply.github.com>
2017-08-10 00:42:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,6 @@ var button = document.getElementById('counter');
 
 button.onclick = function() {
 
-
     // Create a request oblect
     var reques = new XMLHttpRequest();
     
@@ -24,18 +23,26 @@ button.onclick = function() {
      request.send(null);   
 };  
 
-//submit name
+//submit a name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
-submit.onclick = function() {
-    // Make a request to the server and send the name
-    // Capture a list of names and render it as a list
-    var name = ['name1','name2','name3', 'name4', 'name4'];
-    var list = '';
-    for (var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>';
-     }
-var ul = document.getElementById('namelist');
-ul.innerHTML = list;
+        submit.onclick = function() {
+            
+            // Create a request oblect
+            var reques = new XMLHttpRequest();
+            names = JSON.parse(names);
+            var list = '';
+            for (var i=0; i<names.length; i++) {
+                list += '<li>' +names(i)+ '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+            }
+        }
+        // Not done yet
+        };
+    // Make the request    
+     request.open('GET" http://manodharn.imad.hasura-app.io/submit-name?name=',+name, true);  
+     request.send(null); 
 };
