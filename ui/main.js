407e1d5e5f8723f672d7befe37fcf62968c70852
Manodhar-Nainwal <30679208+Manodhar-Nainwal@users.noreply.github.com>
2017-08-17 01:01:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,25 +24,30 @@ button.onclick = function () {
 };  
 
 //submit a name
-
-var submit = document.getElementById('submit_btn');
+     var submit = document.getElementById('submit_btn');
         submit.onclick = function() {
+
+// Create a request oblect
+        var request = new XMLHttpRequest();
             
-            // Create a request oblect
-            var request = new XMLHttpRequest();
-            names = JSON.parse(names);
-            var list = '';
-            for (var i=0; i<names.length; i++) {
-                list += '<li>' +names(i)+ '</li>';
+// capture the response and store it in a variable
+    request.onreadystatchange = function () {
+        if (request.readystat === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;}
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i<names.length; i++) {
+                    list += '<li>' +names(i)+ '</li>';
             }
             var ul = document.getElementById('namelist');
             ul.innerHTML = list;
-            };
-            
+        }
         // Not done yet
+    };
         
-    // Make the request  
-    var nameInput = document.getElementById('name');
+    // Make the request 
+     var nameInput = document.getElementById('name');
     var name = nameInput.value;
      request.open('GET http://manodharn.imad.hasura-app.io/submit-name?name=', +name, true);  
      request.send(null); 
