92d6f74c8ba4f9dcd18d8a85d6559767d13ba3d9
Manodhar-Nainwal <30679208+Manodhar-Nainwal@users.noreply.github.com>
2017-08-16 21:08:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/test_db', function(req, res){
         if (err) {
             res.status(500).send(err.tostring());
         } else {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
 });
