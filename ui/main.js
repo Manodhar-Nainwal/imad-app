2bfdb0db12b6ce51b1f4d57d14201fba3e995902
Manodhar-Nainwal <30679208+Manodhar-Nainwal@users.noreply.github.com>
2017-08-17 10:03:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,6 @@ button.onclick = function () {
             // take some action
            if (request.status === 200) {
                 var counter = request.responseText;
-                counter = counter + 1;
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
@@ -25,32 +24,3 @@ button.onclick = function () {
      request.send(null);   
 };  
 
-//submit a name
-     var submit = document.getElementById('submit_btn');
-        submit.onclick = function() {
-
-// Create a request object
-        var request = new XMLHttpRequest();
-            
-// capture the response and store it in a variable
-    request.onreadystatchange = function () {
-        if (request.readystat === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                var counter = request.responseText;}
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i<names.length; i++) {
-                    list += '<li>' +names(i)+ '</li>';
-            }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
-        }
-        // Not done yet
-    };
-        
-    // Make the request 
-     var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-     request.open('GET http://manodharn.imad.hasura-app.io/counter', true);  
-     request.send(null); 
-};
