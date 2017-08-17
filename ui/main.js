72e9da6309629cdc3518a1c3335fa00b1cdd011b
Manodhar-Nainwal <30679208+Manodhar-Nainwal@users.noreply.github.com>
2017-08-17 12:53:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,15 +34,18 @@ button.onclick = function () {
 // capture the response and store it in a variable
     request.onreadystatchange = function () {
         if (request.readystat === XMLHttpRequest.DONE) {
+            // take some action
             if (request.status === 200) {
-                var names = request.responseText;}
+                // capture a lsit of names and rander it
+                var names = request.responseText;
                 names = JSON.parse(names);
                 var list = '';
                 for (var i=0; i<names.length; i++) {
                     list += '<li>' + names(i) + '</li>';
-            }
+                }
             var ul = document.getElementById('namelist');
             ul.innerHTML = list;
+            }
         }
         // Not done yet
     };
@@ -52,4 +55,5 @@ button.onclick = function () {
      var name = nameInput.value;
      request.open('GET http://manodharn.imad.hasura-app.io/submit-name?name=' + name, true);  
      request.send(null); 
+     
 };
