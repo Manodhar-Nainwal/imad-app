0f05ba83e52d35136b3db71844728fb0fb5a9ccd
Manodhar-Nainwal <30679208+Manodhar-Nainwal@users.noreply.github.com>
2017-08-18 09:25:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,9 +76,9 @@ app.get('/counter', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res) { // URL: /submit-name?name = xxxx
+app.get('/submit-name', function(req, res) { // URL: /submit-name?name=xxxx
 // get the name from the request
-var name = req.params.name;
+var name = req.query.name;
 
 names.push(name);
 // JSON: Javascript Object Notation
