e99f3b07e9c7d606512368867f33185dcbbf1c52
Manodhar-Nainwal <30679208+Manodhar-Nainwal@users.noreply.github.com>
2017-08-24 22:00:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,34 +1,33 @@
 
 //submit username, password to login
-     var submit = document.getElementById('submit_btn');
-        submit.onClick = function() {
+    var submit = document.getElementById('submit_btn');
+    submit.onClick = function() {
 
 // Create a request object
         var request = new XMLHttpRequest();
             
 // capture the response and store it in a variable
     request.onreadystatechange = function () {
-        if (request.readystate === XMLHttpRequest.DONE) {
+        if (request.readyState === XMLHttpRequest.DONE) {
             // take some action
             if (request.status === 200) {
-                // capture a lsit of names and rander it
-                var names = request.responseText;
-                names = JSON.parse(names);
-                var list = '';
-                for (var i=0; i<names.length; i++) {
-                    list += '<li>' + names(i) + '</li>';
-                }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
+                alert('logged in successfully');
+            } else if (request.status === 403) {
+                alert('Username/Password is incorrect');
+            } else if (request.status === 500) {
+                alert('Something went wrong on the server');
             }
         }
         // Not done yet
     };
         
     // Make the request 
-     var nameInput = document.getElementById('name');
-     var name = nameInput.value;
-     request.open('POST', 'http://manodharn.imad.hasura-app.io/submit-name?name=' + name, true);  
-     request.send(JSON.stringyfy({username: username, password: password})); 
+     var username = document.getElementById('username').value;
+     var password = document.getElementById('password').value;
+     console.log(username);
+     console.log(password);
+     request.open('POST', 'http://manodharn.imad.hasura-app.io/login', true);
+     request.setRequestHeader('Content-Type', 'application/json');
+     request.send(JSON.stringyfy({username: username, password: password}));
      
 };
